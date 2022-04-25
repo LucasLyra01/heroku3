e35dd06f651f74bf60812c4250ae0bb2713febcc
LucasLyra01 <lucaslyra8@gmail.com>
2022-04-25 10:54:38 -0300

    v1

---
+++ src/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const port = process.env.PORT
+const hostname = process.env.HOSTNAME
+
+app.use(cors());
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send({
+        message: 'Rodando'
+    })
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Rodando no endereço ${hostname}:${port}`)
+});
