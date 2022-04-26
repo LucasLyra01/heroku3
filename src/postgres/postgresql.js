23dbc2336253f0d2d5d7577b85736d3f30fd54cb
LucasLyra01 <lucaslyra8@gmail.com>
2022-04-26 17:20:59 -0300

    API V9

--- src/postgres/postgresql.js
+++ src/postgres/postgresql.js
@@ -1,24 +1,3 @@
-// const { Pool, Client } = require('pg');
-
-// var client = new Client({
-//     user: process.env.POSTGRE_USER,
-//     password: process.env.POSTGRE_PASSWORD,
-//     database: process.env.POSTGRE_DATABASE,
-//     port: process.env.POSTGRE_PORT,
-//     host: process.env.POSTGRE_HOST,
-//     ssl: true
-// });
-
-// try {
-//     client.connect( () => {
-//         console.log('Conectado')
-//     });
-// } catch (error) {
-//     console.log(error)
-// }
-
-// module.exports = client;
-
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
